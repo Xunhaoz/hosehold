6d28ce3490c66b8ce22b49c4f38e17f20ab0c665
Xunhaoz <leo20020529@gmail.com>
2023-11-07 15:08:33 +0800

    ADD: code_xunhaoz002

---
+++ predictor/code_xunhaoz003.py
@@ -0,0 +1,17 @@
+from dataloader.data_xunhaoz001 import DataXunhaoz001
+from sklearn.model_selection import cross_validate
+from xgboost import XGBRegressor
+
+dl = DataXunhaoz001()
+xgb = XGBRegressor()
+
+train_data, train_label = dl.get_data_label()
+res = cross_validate(
+    xgb, train_data, train_label,
+    cv=5, verbose=1, return_train_score=True, scoring='neg_mean_squared_error'
+)
+
+test_score = -res['test_score'].mean()
+train_score = -res['train_score'].mean()
+
+print(f"{test_score= :.4f}, {train_score= :.4f}")
